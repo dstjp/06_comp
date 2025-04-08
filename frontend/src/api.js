b5549214cd5c5dcb2209f02e934b795a3188c068
Dylano <noob.hyper@gmail.com>
2025-04-08 21:56:12 +0200

    api file created

---
+++ frontend/src/api.js
@@ -0,0 +1,5 @@
+export async function getParts() {}
+export async function getPart() {}
+export async function createPart() {}
+export async function updatePart() {}
+export async function deletePart() {}
