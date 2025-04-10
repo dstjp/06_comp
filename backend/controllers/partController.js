bfc4a4bcafeda407eaf743f5d7e9facf0c4ecbe0
Dylano <167641317+dstjp@users.noreply.github.com>
2025-04-10 10:08:19 +0200

    tracking implemented

--- backend/controllers/partController.js
+++ backend/controllers/partController.js
@@ -2,51 +2,35 @@ const { Component } = require("../models/part");
 
 //Retrieve all parts
 exports.getComponents = async (request, response) => {
-	try {
-		const { componentType, manufacturer } = request.query;
+  try {
+    const { componentType, manufacturer } = request.query;
 
-		const query = {};
-		if (componentType) query.componentType = componentType;
-		if (manufacturer) query.manufacturer = manufacturer;
+    const query = {};
+    if (componentType) query.componentType = componentType;
+    if (manufacturer) query.manufacturer = manufacturer;
 
-		const parts = await Component.find(query);
-		response.status(200).json(parts);
-	} catch (error) {
-		response
-			.status(500)
-			.json({ message: "Error retrieving parts", error: error.message });
-	}
+    const parts = await Component.find(query);
+    response.status(200).json(parts);
+  } catch (error) {
+    response
+      .status(500)
+      .json({ message: "Error retrieving parts", error: error.message });
+  }
 };
 
 //Retrieve one Part (by ID)
 exports.getComponentById = async (request, response) => {
-	try {
-		const part = await Component.findById(request.params.id);
+  try {
+    const part = await Component.findById(request.params.id);
 
-		if (!part) {
-			return response.status(404).json({ message: "Part not found" });
-		}
+    if (!part) {
+      return response.status(404).json({ message: "Part not found" });
+    }
 
-		response.status(200).json(part);
-	} catch (error) {
-		response
-			.status(500)
-			.json({ message: "Error retrieving parts", error: error.message });
-	}
-};
-
-exports.getComponentById = async (request, response) => {
-	try {
-		const part = await Component.findById(request.params.id);
-
-		if (!part) {
-			return response.status(404).json({ message: "Part not found" });
-		}
-
-		response.status(200).json(part);
-	} catch (error) {
-		response
-			.status(500)
-			.json({ message: "Error retrieving parts", error: error.message });
-	}
+    response.status(200).json(part);
+  } catch (error) {
+    response
+      .status(500)
+      .json({ message: "Error retrieving parts", error: error.message });
+  }
 };
