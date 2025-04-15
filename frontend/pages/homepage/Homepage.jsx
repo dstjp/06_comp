60caf90a1424ce0ca9eb0fee96e321e4076ed86b
Dylano <noob.hyper@gmail.com>
2025-04-15 12:56:28 +0200

    homepage styling done

--- frontend/pages/homepage/Homepage.jsx
+++ frontend/pages/homepage/Homepage.jsx
@@ -1,14 +1,26 @@
 import "./homepage.css";
+import PCpic from "./gaming-pc.png";
 
 export function Homepage() {
 	return (
 		<div className="container">
 			<div className="title-container">
-				<p className="title top-title">Build Your</p>{" "}
-				<p className="title bottom-title"> Own PC</p>
+				<div className="title-text-container">
+					<p className="title top-title">Build Your</p>{" "}
+					<p className="title bottom-title"> Own PC</p>
+				</div>
+				<img src={PCpic} alt="computer picture" className="pc-pic" />
+			</div>
+			<div className="text-container">
+				<p className="text">
+					Create your own custom made personal computer with the components
+					available in the database.
+				</p>
 			</div>
 			<div className="button-container">
-				<button className="create-button">Start Creating</button>
+				<a href="/#/createpc" className="create-button">
+					Start Creating
+				</a>
 			</div>
 		</div>
 	);
