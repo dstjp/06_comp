8bfb0058662d3d65f92d84d06024d4d4f4058264
Dylano <noob.hyper@gmail.com>
2025-04-18 13:41:51 +0200

    backto zero

--- backend/connect.js
+++ backend/connect.js
@@ -1,8 +1,7 @@
 const mongoose = require("mongoose");
 require("dotenv").config({ path: "./config.env" });
 
-/* const connectionString = process.env.M_URI; */
-const connectionString = "mongodb://localhost:3000";
+const connectionString = process.env.M_URI;
 
 mongoose.connect(connectionString);
 
