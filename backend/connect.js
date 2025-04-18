7ae33dac158398defdcadac67905094836331a9c
Dylano <noob.hyper@gmail.com>
2025-04-18 13:30:59 +0200

    removed toml file

--- backend/connect.js
+++ backend/connect.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
 require("dotenv").config({ path: "./config.env" });
 
-const connectionString = process.env.M_URI;
+/* const connectionString = process.env.M_URI; */
+const connectionString = "mongodb://localhost:3000";
 
 mongoose.connect(connectionString);
 
