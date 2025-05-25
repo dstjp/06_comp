2d73180de765ffd639497bf3ce4d8b22715bb8f3
Dylano <noob.hyper@gmail.com>
2025-05-25 17:06:31 +0200

    fix order of column

--- backend/routes/buildRoutes.js
+++ backend/routes/buildRoutes.js
@@ -4,10 +4,12 @@ const buildController = require("../controllers/buildController");
 const router = express.Router();
 
 router.post("/", buildController.createBuild);
-router.get("/builds", buildController.getBuild); //Development
-router.get("/:id", buildController.getBuildById); //Development
 router.get("/user/:userId", buildController.getUserBuilds);
 router.put("/:id", buildController.updateBuild);
 router.delete("/:id", buildController.deleteBuild);
+
+//Development
+/* router.get("/builds", buildController.getBuild); 
+router.get("/:id", buildController.getBuildById); */
 
 module.exports = router;
